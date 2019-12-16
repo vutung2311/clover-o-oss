5ad66b64944158c3a4776f5feee6e370c03927c2
Tung Vu <vutung.ct@gmail.com>
2019-12-16 09:50:22 +0700

    Clean up old *.ko before build new flashable zip

--- build.sh
+++ build.sh
@@ -24,63 +24,64 @@ KERNEL_DEFCONFIG=clover-perf_defconfig
 
 FUNC_CLEAN_DTB()
 {
-	if ! [ -d ${RDIR}/arch/${ARCH}/boot/dts ] ; then
-		echo "no directory : "${RDIR}/arch/${ARCH}/boot/dts""
-	else
-		echo "rm files in : "${RDIR}/arch/${ARCH}/boot/dts/*.dtb""
-		rm ${RDIR}/arch/${ARCH}/boot/dts/qcom/*.dtb
-	fi
+    if ! [ -d ${RDIR}/arch/${ARCH}/boot/dts ] ; then
+        echo "no directory : "${RDIR}/arch/${ARCH}/boot/dts""
+    else
+        echo "rm files in : "${RDIR}/arch/${ARCH}/boot/dts/*.dtb""
+        rm ${RDIR}/arch/${ARCH}/boot/dts/qcom/*.dtb
+    fi
 }
 
 FUNC_BUILD_KERNEL()
 {
-	echo ""
-	echo "=============================================="
-	echo "START : FUNC_BUILD_KERNEL"
-	echo "=============================================="
-	echo ""
-	echo "build common config="$KERNEL_DEFCONFIG ""
+    echo ""
+    echo "=============================================="
+    echo "START : FUNC_BUILD_KERNEL"
+    echo "=============================================="
+    echo ""
+    echo "build common config="$KERNEL_DEFCONFIG ""
 
-	FUNC_CLEAN_DTB
+    FUNC_CLEAN_DTB
 
-	make -j$BUILD_JOB_NUMBER ARCH=${ARCH} \
-			CC=$BUILD_CC \
-			LD=$BUILD_LD \
-			CROSS_COMPILE="$BUILD_CROSS_COMPILE" \
-			$KERNEL_DEFCONFIG || exit -1
+    make -j$BUILD_JOB_NUMBER ARCH=${ARCH} \
+            CC=$BUILD_CC \
+            LD=$BUILD_LD \
+            CROSS_COMPILE="$BUILD_CROSS_COMPILE" \
+            $KERNEL_DEFCONFIG || exit -1
 
-	echo ""
-	for var in "$@"
-	do
-		if [[ "$var" = "--with-lto" ]] ; then
-			echo "Enable CLANG_LTO"
-			./scripts/config \
-			-e CLANG_LTO
-			continue
-		fi
-		if [[ "$var" = "--with-supersu" ]] ; then
-			echo "Enable ASSISTED_SUPERUSER"
-			./scripts/config \
-			-e ASSISTED_SUPERUSER
-			continue
-		fi
-	done
-	echo ""
+    echo ""
+    for var in "$@"
+    do
+        if [[ "$var" = "--with-lto" ]] ; then
+            echo "Enable CLANG_LTO"
+            ./scripts/config \
+            -e CLANG_LTO
+            continue
+        fi
+        if [[ "$var" = "--with-supersu" ]] ; then
+            echo "Enable ASSISTED_SUPERUSER"
+            ./scripts/config \
+            -e ASSISTED_SUPERUSER
+            continue
+        fi
+    done
+    echo ""
 
-	make -j$BUILD_JOB_NUMBER ARCH=${ARCH} \
-			CC=$BUILD_CC \
-			LD=$BUILD_LD \
-			CROSS_COMPILE="$BUILD_CROSS_COMPILE" || exit -1
+    make -j$BUILD_JOB_NUMBER ARCH=${ARCH} \
+            CC=$BUILD_CC \
+            LD=$BUILD_LD \
+            CROSS_COMPILE="$BUILD_CROSS_COMPILE" || exit -1
 
-	echo ""
-	echo "================================="
-	echo "END   : FUNC_BUILD_KERNEL"
-	echo "================================="
-	echo ""
+    echo ""
+    echo "================================="
+    echo "END   : FUNC_BUILD_KERNEL"
+    echo "================================="
+    echo ""
 }
 
 FUNC_BUILD_RAMDISK()
 {
+    find ${RDIR}/out -name "*.ko" -exec rm {} \;
     cp ${RDIR}/arch/${ARCH}/boot/Image.gz-dtb ${RDIR}/aik/split_img/boot.img-zImage
     find ${RDIR} -name "*.ko" -not -path "*/aik/ramdisk/*" -exec cp -f {} ${RDIR}/out/system/lib/modules/ \;
     cd ${RDIR}/aik
@@ -90,23 +91,23 @@ FUNC_BUILD_RAMDISK()
 
 FUNC_BUILD_ZIP()
 {
-	cd ${RDIR}/out/
-	cp ${RDIR}/aik/image-new.img ${RDIR}/out/boot.img
-	cd ${RDIR}/out/ && zip ../${OUTPUT_ZIP}.zip -r *
+    cd ${RDIR}/out/
+    cp ${RDIR}/aik/image-new.img ${RDIR}/out/boot.img
+    cd ${RDIR}/out/ && zip ../${OUTPUT_ZIP}.zip -r *
 }
 
 # MAIN FUNCTION
 rm -rf ./build.log
 (
-	START_TIME=`date +%s`
+    START_TIME=`date +%s`
 
-	FUNC_BUILD_KERNEL "$@"
-	FUNC_BUILD_RAMDISK
-	FUNC_BUILD_ZIP
+    FUNC_BUILD_KERNEL "$@"
+    FUNC_BUILD_RAMDISK
+    FUNC_BUILD_ZIP
 
-	END_TIME=`date +%s`
+    END_TIME=`date +%s`
 
-	let "ELAPSED_TIME=${END_TIME}-${START_TIME}"
-	echo "Total compile time was ${ELAPSED_TIME} seconds"
+    let "ELAPSED_TIME=${END_TIME}-${START_TIME}"
+    echo "Total compile time was ${ELAPSED_TIME} seconds"
 
 ) 2>&1	 | tee -a ./build.log
